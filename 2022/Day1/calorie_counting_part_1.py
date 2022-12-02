23d250b0fe18f90f4b7332673c9d4a96560b3d59
Tim Pogue <t.pogue.python@gmail.com>
2022-12-02 07:50:04 -0600

    Update part 1

--- 2022/Day1/calorie_counting_part_1.py
+++ 2022/Day1/calorie_counting_part_1.py
@@ -2,9 +2,58 @@ class Elf:
     def __init__(self, calories: list):
         self.calorie_list = calories
 
+    def __repr__(self):
+        return f"Elf({self.calorie_list})"
+
+    def __str__(self):
+        return str(self.__repr__())
+
     @property
     def calories(self):
         return sum(self.calorie_list)
 
+    def __gt__(self, other):
+        if self.calories > other.calories:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.calories < other.calories:
+            return True
+        else:
+            return False
+
 
 def read_calories_packs():
+    f = open("data.txt", 'r')
+
+    try:
+        while True:
+            calories = []
+            calorie = f.readline()
+
+            while calorie != "\n":
+                if calorie != "":
+                    calories.append(int(calorie))
+                calorie = f.readline()
+
+            yield Elf(calories)
+            continue
+
+    except EOFError:
+        f.close()
+
+
+def main():
+    most_elf = Elf([])
+
+    for elf in read_calories_packs():
+        if elf > most_elf:
+            most_elf = elf
+
+    print(most_elf.calories)
+
+
+if __name__ == "__main__":
+    main()
